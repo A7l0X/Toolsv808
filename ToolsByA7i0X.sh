f87259bec0ac105c8d9ef11c2674c86f3ccb4855
sempak404 <54473839+sempak404@users.noreply.github.com>
2019-10-09 18:44:29 +0700

    Add files via upload
    Please Jangan Di Record Bangst Cape Gw Bikin nya Jadi Hargain Yg Bikin Tools Ini Lu Jangan Tinggal Enak nya Doang

---
+++ ToolsByA7i0X.sh
@@ -0,0 +1,258 @@
+clear
+username(){
+echo -n "${i}Masukkan Username: "
+read user
+echo -n "${i}Masukkan Password: "
+read  pass
+case $user in
+'SHAM')
+password $pass
+;;
+*) echo "${me}USER & PASS = SALAH Qimak!!"
+username
+;;
+esac
+}
+password(){
+case $1 in
+        'FA808')
+                echo "password benar"
+                ;;
+        *) echo "${me} PASSWORD = SALAH Qimak!!"
+                username
+                ;;
+                esac
+
+}
+username
+
+clear
+echo "╔╦╦╦╦╦╦╦╦╦╦╗" | lolcat
+echo "╠╬╬╬╬╬╬╬╬╬╬╣" | lolcat
+echo "╠╬╬█╬╬╬╬█╬╬╣" | lolcat
+echo "╠╬╬╬╬╬╬╬╬╬╬╣" | lolcat
+echo "╠╬████████╬╣" | lolcat
+echo "╠╬█╬╬╬╬╬╬█╬╣" | lolcat
+echo "╚╩╩╩╩╩╩╩╩╩╩╝" | lolcat
+
+echo 
+echo
+echo "<============================>" | lolcat
+echo " Nama        : FA808" | lolcat
+echo " AUTHOR      : SHAM A7i0X" | lolcat
+echo " TngglDbuat  : 9/Oktober/2019 9.30" | lolcat
+echo "<============================>" | lolcat
+
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 1. Dark Fb v1.6" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 2. Tools Sempak1.1" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 3. Install Go Dork" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 4. Chek IP Sendiri" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 5. Boom Sms/Call" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 6. Send Trojan" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 7. Sadap Wa" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 8. Install BootChat" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 9. Scan IP Website" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 10. hack Efbeh" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 11. ToolsSpamSms" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 12. Spam OTP" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 13. SPAM SMS TRI" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 14. SPAM SMS TRIv2" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 15. Spam Sms" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 16. Mod Caping" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 17. Spam Santet-ONLINE" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 18. Spam GCOspam" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 19. LITESPAM" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 20. Spam TERBARU Lho" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 21. Spammer BlackHeat" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 22. Spam Spazz" | lolcat
+echo "╞═════════════════════════════════════════╕" | lolcat
+echo "| 0. exit"
+echo "---------"
+read -p "----" pil
+
+case $pil in
+
+1) git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+
+;;
+
+2) git clone https://github.com/sempak404/Toolsv1.1
+cd Toolsv1.1
+ls
+bash IAFv1.1.sh
+
+;;
+
+3) git clone https://github.com/Bl4ckDr460n/G-Dork
+cd G-Dork
+python2 G-Dork.py
+
+;;
+
+4) git clone https://github.com/Bl4ckDr460n/SPAMer
+cd SPAMer
+python2 SPAMer.py
+
+;;
+
+5) git clone https://github.com/Bl4ckDr460n/My-Ip
+cd My-Ip
+python2 My-Ip.py
+
+;;
+
+6) git clone https://github.com/Bl4ckDr460n/STrojans
+cd STrojans
+python2 STrojans.py
+
+;;
+
+7) git clone https://github.com/Bl4ckDr460n/HxWhatsApp
+cd HxWhatsApp
+python2 HxWhatsApp.py
+
+;;
+
+8) git clone https://github.com/Bl4ckDr460n/BotChat
+cd BotChat
+python2 BotChat.py
+
+;;
+
+9) git clone https://github.com/Bl4ckDr460n/ScanIp
+cd ScanIp
+ls
+python2 ScanIp
+
+;;
+
+10) pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install php
+pkg install git
+pip2 install mechanize
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+ls
+sh new.sh
+
+;;
+
+11) apt update && apt upgrade
+apt install git
+apt install php
+apt install nano
+apt install python2
+pip2 install requests
+pip2 install mechanize
+git clone  https://github.com//SpamSms.git
+cd SpamSms
+python2 mantan.py
+
+;;
+
+12) git clone https://www.github.com/Cvar1984/LiteOTP
+cd liteOTP
+chmod 777 lite.php
+echo "Cara Menjalankannya"
+echo "php lite.php no kalean di awali 62"
+php lite.php
+
+;;
+
+13) git clone https://github.com/hatakecnk/SPAMTRI2
+cd SPAMTRI2
+php spam-tri.php
+
+;;
+
+14) git clone https://github.com/hatakecnk/SPAMTRI
+cd SPAMTRI
+php spam-tri.php
+
+;;
+
+15) git clone https://github.com/Senitopeng/SpamSms.git
+cd SpamSms
+
+;;
+
+16) git clone https://github.com/anggaid14/caping
+cd caping
+php run.php
+
+;;
+
+17) git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python2 santet.py
+
+;;
+
+18) git clone https://github.com/Amriez/gcospam
+cd gcospam
+sh install.sh
+sh gco.sh
+
+;;
+
+19) git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+bash LITESPAM.sh
+
+;;
+
+20) git clone https://github.com/haijuga7/New-Spammer
+cd New-Spammer
+unzip Spamm.zip
+sh spam.sh
+
+;;
+
+21) git clone https://github.com/BLACheat78/spammblackheat78
+cd spammblackheat78
+bash Blackspamm.sh
+
+;;
+
+22) git clone https://github.com/Gameye98/SpazSMS
+cd SpazSMS
+ls
+python2 lite.py
+
+;;
+
+
+0) echo "Mau Kemana??"
+echo "Jangan Tinggalin Aku"
+echo "Kalau Mau Keluar Yaudh Gpp"
+echo "Sampai Jumpa Lagi :)"
+exit
+esac
+
+
